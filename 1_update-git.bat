b7e00ff77fca0fb8e0e9e3c9ef1ce6d615be0994
SanXiaoXing <1137199378@qq.com>
2024-09-07 02:24:57 +0800

    add(image): 07-2024-09 周六:24

--- 1_update-git.bat
+++ 1_update-git.bat
@@ -1,18 +1,18 @@
 @echo off
 
 :: 获取当前时间，格式为 YYYY-MM-DD HH:MM
-for /f "tokens=1-5 delims=/-: " %%a in ('echo %date% %time%') do (
+for /f "tokens=1-4 delims=/-: " %%a in ('echo %date% %time%') do (
     set year=%%c
     set month=%%a
     set day=%%b
     set hour=%%d
-    set minute=%%e
 )
 
+set minute=%time:~3,2%
 set current_time=%year%-%month%-%day% %hour%:%minute%
 
 :: 设置提交信息
-set commit_message=add(blog): %current_time%
+set commit_message=add(image): %current_time%
 
 :: 添加所有更改到暂存区
 git add .
@@ -22,3 +22,6 @@ git commit -m "%commit_message%"
 
 :: 推送更改到远程仓库
 git push origin main
+
+:: 等待用户按任意键退出
+pause
