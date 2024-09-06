75816eb40ecca3917545743d1e266fd050099e60
SanXiaoXing <1137199378@qq.com>
2024-09-07 02:21:06 +0800

    add(blog): 07-2024-09 周六:2

---
+++ 1_update-git.bat
@@ -0,0 +1,24 @@
+@echo off
+
+:: 获取当前时间，格式为 YYYY-MM-DD HH:MM
+for /f "tokens=1-5 delims=/-: " %%a in ('echo %date% %time%') do (
+    set year=%%c
+    set month=%%a
+    set day=%%b
+    set hour=%%d
+    set minute=%%e
+)
+
+set current_time=%year%-%month%-%day% %hour%:%minute%
+
+:: 设置提交信息
+set commit_message=add(blog): %current_time%
+
+:: 添加所有更改到暂存区
+git add .
+
+:: 提交更改并使用当前时间作为提交信息
+git commit -m "%commit_message%"
+
+:: 推送更改到远程仓库
+git push origin main
